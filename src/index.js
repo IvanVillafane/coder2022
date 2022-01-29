fc0804a530c4047b2238534a59be90ac45e6e864
IvanVillafane <matheobenjamin99@gmail.com>
2022-01-29 00:27:01 -0300

    entrega

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './src/App';
-import reportWebVitals from './src/reportWebVitals';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,12 +16,7 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
 
-const express = require ("express");
-const app = express ();
-const port = process.env.PORT  || 4000;
-app.listen(port);
-app.get("/",(req, res, )=>{res.send("la pagina Inicio");});
-console.log( `Listen on Port ${port}`);
+
 
 
 reportWebVitals(console.log);
